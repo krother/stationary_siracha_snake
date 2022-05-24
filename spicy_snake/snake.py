d1b5e414a49b4cd6982ff65c2d5dc681667174f6
Kristian Rother <krother@academis.eu>
2022-05-24 22:53:45 +0200

    #2 include playground in main game

--- spicy_snake/snake.py
+++ spicy_snake/snake.py
@@ -1,3 +1,5 @@
+
+from spicy_snake.playground import Playground
 import curses
 
 
@@ -12,6 +14,7 @@ KEY_COMMANDS = {97: LEFT, 100: RIGHT, 119: UP, 115: DOWN}
 screen = curses.initscr()
 curses.start_color()
 curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
 curses.curs_set(0)
 curses.noecho()
 curses.raw()
@@ -24,9 +27,15 @@ win.nodelay(True)
 def game_loop(screen):
     x, y = 5, 5
 
+    pg = Playground(30, 14)
+
     # draw
     screen.clear()
     screen.addch(y, x, "O", curses.color_pair(1))
+    for x in range(31):
+        for y in range(15):
+            if pg.is_obstacle((x, y)):
+                screen.addch(y, x, "#", curses.color_pair(2))
     win.refresh()
     screen.refresh()
 
@@ -44,6 +53,12 @@ def game_loop(screen):
             screen.addch(y, x, "O", curses.color_pair(1))
             win.refresh()
             screen.refresh()
+            for x in range(31):
+                for y in range(15):
+                    if pg.is_obstacle((x, y)):
+                        screen.addch(y, x, "#", curses.color_pair(2))
+
+
 
 
 if __name__ == "__main__":
