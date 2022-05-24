c8df2d919da81365d0a743d52fce53a9c40d9f8f
Kristian Rother <krother@academis.eu>
2022-05-24 12:05:34 +0200

    #2 enumerate tests for playground

---
+++ tests/test_playground.py
@@ -0,0 +1,13 @@
+"""
+Tests for the Playground class:
+
+- creating a Playground object works
+- after creating a Playground object it has the correct size attribute
+- food should be inside the Playground size
+- add_random_food puts the food in random positions
+- food is not placed on obstacles (boundaries)
+- is_obstacle for a coordinate inside gives True
+- is_obstacle for a coordinate at the boundary gives False
+"""
+
+...
